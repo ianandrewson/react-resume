d87cbbe41d312c6b2110d0daf15c8abb0df5f2c3
Ian Andrewson <ianandrewson@gmail.com>
2020-02-02 22:52:43 -0800

    details of skeleton filled out; resume written. Ready to style

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -6,12 +6,35 @@ export default function Sidebar() {
     <>
       <div>
         <h3>SUMMARY</h3>
+        <p>
+          Full Stack Software Developer with a background in audio production and aeronautics QA. Proponent of quality, tested code with a penchant for communication and problem solving. Proven self-learner, humble pupil, and quickly adaptable. Analog Synth enthusiast.
+        </p>
       </div>
       <div>
         <h3>CONTACT DETAILS</h3>
+        <p> Mobile: xxx-xxx-xxxx </p>
+        <p> Email: xxnxxxxxxxxx@xxxxx.xxx </p>
+        <a href='github.com/xanxxdrexsox'>Github</a>
+        <br/>
+        <a href='linkedin.com/in/xanxxdrexsox'>Linkedin</a>
       </div>
       <div>
         <h3>TECH STACK</h3>
+        <p>
+          Languages: JavaScript, HTML, CSS. Dabbled in Python, C++, and C#
+        </p>
+        <p>
+          Environments & Frameworks:  Node.js, Express, Mongoose, React, Redux, Processing/P5
+        </p>
+        <p>
+          Databases: MongoDB, Postgres
+        </p>
+        <p>
+          Testing: Jest, Q-unit, TDD
+        </p>
+        <p>
+          Other:  Git, Github, Heroku, VSCode, TravisCI, Compass, Postman
+        </p>
       </div>
     </>
   );
